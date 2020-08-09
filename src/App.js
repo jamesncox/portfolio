7fc454bf2a8d9eb0769437800fae971957b117d3
jamesncox <jamesncox@yahoo.com>
2020-08-09 18:28:31 -0500

    Delete  Parallax.css and create Shapes.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import './App.css';
+import './App.css'
+// import './Shapes.css'
 import Home from './components/layout/Home'
 
 function App() {
   return (
     <div className="App">
       <Home />
+
+
+      {/* <div className="area" >
+        <ul className="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div > */}
+
     </div>
   );
 }
