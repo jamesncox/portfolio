2f38419d9e4e9cda210ab3603e86d6f873920355
jamesncox <jamesncox@yahoo.com>
2020-08-09 17:34:39 -0500

    Update Welcome.js with Typography and styling

--- src/components/layout/Welcome.js
+++ src/components/layout/Welcome.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import EllieAndJames from '../../assets/images/ellie and james.jpg'
 
@@ -9,17 +10,32 @@ const useStyles = makeStyles((theme) => ({
         marginTop: theme.spacing(4),
         maxWidth: "100%",
         height: "auto",
+        // clipPath: "polygon(15% 0%, 100% 0%, 85% 100%, 0% 100%)"
     },
     image: {
         height: 500,
-        borderRadius: ".5rem",
-        transform: "skewY(-11deg)",
+        borderRadius: ".2rem",
+        // transform: "skewY(5deg)",
         [theme.breakpoints.down('sm')]: {
             height: 450
         },
         [theme.breakpoints.down('xs')]: {
             height: 400
         }
+    },
+    text: {
+        alignSelf: "center",
+        textAlign: "center",
+        fontFamily: "'Jura', sans-serif",
+        color: "#f50057",
+        fontSize: "1.5rem",
+        padding: ".25rem",
+        [theme.breakpoints.down('sm')]: {
+            fontSize: "1.25rem"
+        },
+        [theme.breakpoints.down('xs')]: {
+            fontSize: "1rem"
+        },
     }
 }));
 
@@ -27,7 +43,10 @@ const useStyles = makeStyles((theme) => ({
 export default function Welcome(props) {
     const classes = useStyles()
     return (
-        <Box className={classes.box} >
+        <Box className={classes.box}>
+            <Typography className={classes.text}>
+                Hi. I'm James and I make things.
+            </Typography>
             <img className={classes.image} src={EllieAndJames} alt="Ellie and James" />
         </Box>
     )
