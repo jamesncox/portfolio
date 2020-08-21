cebcd794f2cdaa5af9a4eccd2ae058fc97ce7e4b
jamesncox <jamesncox@yahoo.com>
2020-08-21 09:34:19 -0500

    Stub out BlogPosts card design

--- src/components/Blog/BlogPosts.js
+++ src/components/Blog/BlogPosts.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import CardHeader from '@material-ui/core/CardHeader';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 800,
+        alignSelf: "center",
+        margin: theme.spacing(2)
+    },
+    media: {
+        height: 140,
+    },
+}));
 
 export default function BlogPosts() {
+    const classes = useStyles();
 
     return (
-        <div>THESE ARE SOME BLOG POSTS!</div>
-    )
+        <Card className={classes.root}>
+            <CardHeader
+                avatar={
+                    <Avatar aria-label="recipe" className={classes.avatar}>
+                        R
+                    </Avatar>
+                }
+                title="Shrimp and Chorizo Paella"
+            />
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    title="Contemplative Reptile"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                        across all continents except Antarctica
+          </Typography>
+                </CardContent>
+            </CardActionArea>
+
+        </Card>
+    );
 }
